85d91f687f7f807100c24acd0f2d897db152f9ef
Juho Järvi <juho.jaervi@gmail.com>
2023-02-06 15:06:26 +0200

    main tiedoston siistiminen turhat otettu pois

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,70 +1,58 @@
 import 'package:app/pages/main_page.dart';
 import 'package:flutter/material.dart';
 
+/**
+ * Kalenteri apin main tiedosto
+ * Käsittelee apin käynnistyksen ja navigoinnin
+ */
 void main() {
   runApp(const MyApp());
 }
 
+/**
+ * Apin pääluokka
+ * sisältää oletus teeman ja routerin navigointiin
+ */
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      initialRoute: '/pages',
+      title: 'Kalenteri',
+      initialRoute: '/',
       routes: {
         '/mainpage': (context) => const MainPage(),
       },
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Demo'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+/**
+ * Väliaikainen luokka alkunäkymälle, jonka avulla voi testata omia näkymiä
+ */
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required String title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('Testi'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/mainpage');
+          },
+          child: const Text('nappula :D'),
         ),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        Navigator.pushNamed(context, '/mainpage');
-      }),
     );
+    throw UnimplementedError();
   }
 }
