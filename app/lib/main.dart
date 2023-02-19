bade02a9d1ca3a196cbb10869b86adef9b012a01
weckshuz <siiri-maija.h.weckberg@student.jyu.fi>
2023-02-19 14:00:52 +0200

    Muokattu pubsec-tiedosto

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,63 @@
+
+import 'package:app/pages/main_page.dart';
+
+import 'package:app/pages/login.dart';
+import 'package:app/pages/main_page.dart';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:app/pages/profile.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-Future main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-
-  runApp(MyApp());
+/// Kalenteri apin main tiedosto
+/// Käsittelee apin käynnistyksen ja navigoinnin
+void main() {
+  initializeDateFormatting().then((_) => runApp(const MyApp()));
 }
 
+/// Apin pääluokka
+/// sisältää oletus teeman ja routerin navigointiin
 class MyApp extends StatelessWidget {
-  static final String title = 'User Profile';
+  const MyApp({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kalenteri',
+      initialRoute: '/',
+      routes: {
+
+        '/mainpage': (context) => const MainPage(),
+        '/loginpage': (context) => const LoginPage(),
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Demo'),
+    );
+  }
+}
+
+/// Väliaikainen luokka alkunäkymälle, jonka avulla voi testata omia näkymiä
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key, required String title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Testi'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/loginpage');
+          },
+          child: const Text('testi'),
+        ),
+      ),
+    );
+  }
+
+}//Profiilinäkymä
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,3 +67,4 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
