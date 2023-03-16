f0467e3cebd8dfb247482c370cc3a378d2aa8a22
weckshuz <siiri-maija.h.weckberg@student.jyu.fi>
2023-03-16 15:42:54 +0200

    Removed navbar: didn't work as it should and no time to fix it now

--- app/lib/pages/friends_page.dart
+++ app/lib/pages/friends_page.dart
@@ -1,5 +1,4 @@
 import 'package:vapaat/widgets/button_widget.dart';
-import 'package:vapaat/widgets/navbar_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:vapaat/pages/models/friend.dart';
 import 'package:vapaat/utils/friends_preference.dart';
@@ -7,7 +6,6 @@ import 'package:vapaat/properties.dart';
 import 'package:vapaat/widgets/freetime_widget.dart';
 import 'package:vapaat/pages/settings_page.dart';
 import 'dart:io';
-import 'package:vapaat/widgets/button_widget.dart';
 
 class Friends extends StatefulWidget {
   @override
@@ -17,27 +15,13 @@ class Friends extends StatefulWidget {
 class _FriendsState extends State<Friends> {
   List<Friend> _friendDataList = friendDataList;
   final _scrollController = ScrollController();
-  int _selectedIndex = 0;
-  //Screens from where the bar navigates (copy from main-page)
-  static List<Widget> _widgetOptions = <Widget>[
-    Freetime(),
-    Text('Todo: calender, add/delete events'),
-    SettingsPage(), //from here one can navigate to profile, groups, friends, etc
-  ];
-
-  //when the bar is clicked, the screen changes
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          friend_caption,
+          app_name,
           textAlign:
               TextAlign.center, //does not align in the center yet, fix later
         ),
@@ -82,12 +66,8 @@ class _FriendsState extends State<Friends> {
               ),
             ],
           ),
-          const SizedBox(height: 24),
+          const SizedBox(height: 34),
         ],
-      ),
-      bottomNavigationBar: OwnNavBar(
-        selectedIndex: _selectedIndex,
-        onItemSelected: _onItemTapped,
       ),
     );
   }
