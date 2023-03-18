8dcb6382730f6bd8b9c5ecd52cc0367e0cdf0915
weckshuz <siiri-maija.h.weckberg@student.jyu.fi>
2023-03-18 17:56:40 +0200

    More text in properties.dart

--- app/lib/properties.dart
+++ app/lib/properties.dart
@@ -57,6 +57,10 @@ const group_exit = "Leave group";
 const calender_add = "Add event";
 const calender_delete = "Delete event";
 
+//Freetime page:
+const freetime_update = "Update";
+const freetime_clear = "Clear";
+
 //Logout
 const logout = "Logout";
 
