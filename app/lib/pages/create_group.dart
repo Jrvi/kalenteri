15d4fa194a24e9ad46fa4013d16fbbe5dc4928ac
weckshuz <siiri-maija.h.weckberg@student.jyu.fi>
2023-03-22 11:28:15 +0200

    Now if user have no friends yet, the group creating page will tell user to add friend

--- app/lib/pages/create_group.dart
+++ app/lib/pages/create_group.dart
@@ -57,123 +57,132 @@ class CreateGroupState extends State<CreateGroup> {
           textAlign: TextAlign.center,
         ),
       ),
-      body: ListView(
-        controller: _scrollController,
-        padding: EdgeInsets.symmetric(horizontal: 10),
-        physics: BouncingScrollPhysics(),
-        children: [
-          const SizedBox(height: 24),
-          SizedBox(
-            width: 300,
-            child: TextField(
-              controller: _groupNameController,
-              decoration: InputDecoration(
-                labelText: create_name,
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(12),
-                ),
+      body: _friends.isEmpty // check if there are no friends
+          ? Center(
+              child: Text(
+                'You have not yet added any friends. Add friends so that you can create a group.',
+                textAlign: TextAlign.center,
               ),
-            ),
-          ),
-          SizedBox(height: 34),
-
-          Text(
-            create_members,
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
-          ),
-
-          SizedBox(height: 24),
-
-          //valitut kaverit, nyt scrollaantuu horisontaalisesti, korjattava
-          Container(
-            height: 30,
-            child: ListView.builder(
-              controller: _selectedFriendsScrollController,
-              scrollDirection: Axis.horizontal,
-              itemCount: _selectedFriends.length,
-              itemBuilder: (context, index) {
-                final friendName = _selectedFriends[index];
-                return Chip(
-                  label: Text(friendName),
-                  deleteIcon: Icon(Icons.cancel),
-                  onDeleted: () {
-                    setState(() {
-                      _selectedFriends.remove(friendName);
-                    });
-                  },
-                );
-              },
-            ),
-          ),
-          SizedBox(height: 24),
-
-          //kaverilista
-          SizedBox(
-            height: 380,
-            //jos kavereita enemmän kuin 4, scrollattava alue
-            child: _friends.length > 6
-                ? Scrollbar(
-                    controller: _scrollController,
-                    thumbVisibility: true,
-                    child: ListView.builder(
-                      itemCount: _friends.length,
-                      itemBuilder: (context, index) {
-                        final friend = _friends[index];
-
-                        //Kaverin kuva näkyviin
-                        return ListTile(
-                          leading: CircleAvatar(
-                            backgroundImage: NetworkImage(friend.imagePath),
-                          ),
-                          title: Text(friend.name),
-                          subtitle: Text(friend.email),
-                          onTap: () {
-                            // TODO: Implement selecting friend functionality
-                            _addFriendToSelected(friend.name);
-                          },
-                        );
-                      },
+            )
+          : ListView(
+              controller: _scrollController,
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              physics: BouncingScrollPhysics(),
+              children: [
+                const SizedBox(height: 24),
+                SizedBox(
+                  width: 300,
+                  child: TextField(
+                    controller: _groupNameController,
+                    decoration: InputDecoration(
+                      labelText: create_name,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
                     ),
-                  )
-                : ListView.builder(
-                    scrollDirection: Axis.vertical,
-                    shrinkWrap: true,
-                    itemCount: _friends.length,
+                  ),
+                ),
+                SizedBox(height: 34),
+
+                Text(
+                  create_members,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+
+                SizedBox(height: 24),
+
+                //valitut kaverit, nyt scrollaantuu horisontaalisesti, korjattava
+                Container(
+                  height: 30,
+                  child: ListView.builder(
+                    controller: _selectedFriendsScrollController,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: _selectedFriends.length,
                     itemBuilder: (context, index) {
-                      final friend = _friends[index];
-                      return ListTile(
-                        title: Text(friend.name), //kaverin kuva näkyviin?
-                        onTap: () {
-                          // TODO: Implement selecting friend functionality
-                          _addFriendToSelected(friend.name);
+                      final friendName = _selectedFriends[index];
+                      return Chip(
+                        label: Text(friendName),
+                        deleteIcon: Icon(Icons.cancel),
+                        onDeleted: () {
+                          setState(() {
+                            _selectedFriends.remove(friendName);
+                          });
                         },
                       );
                     },
                   ),
-          ),
-          SizedBox(height: 24),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              TextButton(
-                onPressed: () {
-                  //TODO: skip painikkeelle toiminnot; hylkää jos ryhmälle on jo nimi ja jäsenet?
-                },
-                child: Text(create_cancel),
-              ),
-              SizedBox(width: 64),
-              ElevatedButton(
-                onPressed: () {
-                  //TODO: Luo ryhmä ja tallennsa se
-                  String groupName = _groupNameController.text;
-                  print(': $groupName');
-                },
-                child: Text(create_save),
-              ),
-            ],
-          ),
-        ],
-      ),
+                ),
+                SizedBox(height: 24),
+
+                //Friendlist
+                SizedBox(
+                  height: 380,
+
+                  //If tehre is more than  friends; scrollable
+                  child: _friends.length > 6
+                      ? Scrollbar(
+                          controller: _scrollController,
+                          thumbVisibility: true,
+                          child: ListView.builder(
+                            itemCount: _friends.length,
+                            itemBuilder: (context, index) {
+                              final friend = _friends[index];
+
+                              //Kaverin kuva näkyviin
+                              return ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage:
+                                      NetworkImage(friend.imagePath),
+                                ),
+                                title: Text(friend.name),
+                                subtitle: Text(friend.email),
+                                onTap: () {
+                                  // TODO: Implement selecting friend functionality
+                                  _addFriendToSelected(friend.name);
+                                },
+                              );
+                            },
+                          ),
+                        )
+                      : ListView.builder(
+                          scrollDirection: Axis.vertical,
+                          shrinkWrap: true,
+                          itemCount: _friends.length,
+                          itemBuilder: (context, index) {
+                            final friend = _friends[index];
+                            return ListTile(
+                              title: Text(friend.name), //kaverin kuva näkyviin?
+                              onTap: () {
+                                // TODO: Implement selecting friend functionality
+                                _addFriendToSelected(friend.name);
+                              },
+                            );
+                          },
+                        ),
+                ),
+                SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        //TODO: skip painikkeelle toiminnot; hylkää jos ryhmälle on jo nimi ja jäsenet?
+                      },
+                      child: Text(create_cancel),
+                    ),
+                    SizedBox(width: 64),
+                    ElevatedButton(
+                      onPressed: () {
+                        //TODO: Luo ryhmä ja tallennsa se
+                        String groupName = _groupNameController.text;
+                        print(': $groupName');
+                      },
+                      child: Text(create_save),
+                    ),
+                  ],
+                ),
+              ],
+            ),
     );
   }
 }
